e43640d7d8fc1925f58395ba3e476acda5544061
SamuraiT <t.yasukawa01@gmail.com>
2022-06-05 21:11:38 +0900

    initial file

---
+++ test/sample-test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe("HenkakuBadge", function () {
+  it("sample", async function () {
+    const Badge = await ethers.getContractFactory("HenkakuBadge");
+    const badge = await Badge.deploy();
+    await badge.deployed();
+  });
+});
