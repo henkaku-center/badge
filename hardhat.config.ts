466efed9f9fae29bb90beab561e321f9012486b1
SamuraiT <t.yasukawa01@gmail.com>
2022-06-07 23:37:51 +0900

    feat: Update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,24 +30,24 @@ export default {
   },
   networks: {
     rinkeby: {
-      url: process.env.STAGING_ALCHEMY_KEY,
+      url: process.env.STAGING_ALCHEMY_KEY || '',
       accounts: [process.env.PRIVATE_KEY],
     },
     goerli: {
-      url: process.env.GOERLI_KEY,
+      url: process.env.GOERLI_KEY || '',
       accounts: [process.env.PRIVATE_KEY],
     },
     mainnet: {
       chainId: 137,
-      url: process.env.PROD_ALCHEMY_KEY,
+      url: process.env.PROD_ALCHEMY_KEY || '',
       accounts: [process.env.PRIVATE_KEY]
     }
   },
   etherscan: {
     apiKey: {
-      rinkeby: process.env.ETHERSCAN_API_KEY,
-      goerli: process.env.ETHERSCAN_API_KEY,
-      polygon: process.env.POLYGONSCAN_API_KEY
+      rinkeby: process.env.ETHERSCAN_API_KEY || '',
+      goerli: process.env.ETHERSCAN_API_KEY || '',
+      polygon: process.env.POLYGONSCAN_API_KEY || ''
     }
   }
 };
