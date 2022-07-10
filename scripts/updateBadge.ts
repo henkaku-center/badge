8fc130f5dc09b3c4d50c9f261459550df4b422aa
SamuraiT <4782857+SamuraiT@users.noreply.github.com>
2022-07-10 20:23:40 +0900

    udpdate: file link

--- scripts/updateBadge.ts
+++ scripts/updateBadge.ts
@@ -10,11 +10,22 @@ const main = async () => {
     transferable: false,
     amount: ethers.utils.parseUnits('1000', 18),
     maxSupply: 20,
-    tokenURI: 'https://bafybeienzcfnwqqw55wzjipfod2vhnubj7f3ektmew6rgsdceiea5fe6uq.ipfs.dweb.link/joi-book-1.json',
+    tokenURI: 'https://bafkreibpzhoj32pmacmxydzy6jp6yl2kmpzymkozbofmt2qigkpxbgu56u.ipfs.dweb.link/?filename=joi-book-1.json',
     maxMintPerWallet: 1,
   };
 
+  const welcomeBadge = {
+    mintable: true,
+    transferable: true,
+    amount: ethers.utils.parseUnits('100', 18),
+    maxSupply: 2500,
+    tokenURI: 'https://bafkreiao42z7kyk3qf3gztcoffo7h4haqguucfwaofau2qizwtypq7t44q.ipfs.dweb.link/?filename=welcome-to-henkaku.json',
+    maxMintPerWallet: 3,
+  };
+
+
   await badgeContract.updateBadgeAttr(1, book.mintable, book.tokenURI);
+  await badgeContract.updateBadgeAttr(2, welcomeBadge.mintable, welcomeBadge.tokenURI);
   console.log('finish updating')
 }
 
