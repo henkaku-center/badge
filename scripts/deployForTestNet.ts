24cf2e231eeacfef16a7fe4451193477794120dc
Masumi Kawasaki <geeknees@gmail.com>
2022-12-05 20:54:21 +0900

    update testnet deploy script

--- scripts/deployForTestNet.ts
+++ scripts/deployForTestNet.ts
@@ -1,9 +1,10 @@
-import { ethers } from 'hardhat';
+import { ethers } from 'hardhat'
 
 const main = async () => {
-  const Badge = await ethers.getContractFactory('HenkakuBadge');
+  const Badge = await ethers.getContractFactory('HenkakuBadge')
   const henkakuToken = '0x02Dd992774aBCacAD7D46155Da2301854903118D'
-  const badgeContract = await Badge.deploy(henkakuToken);
+  const fundAddress = '0xF3fb03B582bd19CfA6728FDED7e130aad396d99E'
+  const badgeContract = await Badge.deploy(henkakuToken, fundAddress)
   await badgeContract.deployed()
   console.log('badge contract', badgeContract.address)
 
@@ -12,24 +13,30 @@ const main = async () => {
     transferable: false,
     amount: 0,
     maxSupply: 100,
-    tokenURI: 'https://ikzttp.mypinata.cloud/ipfs/QmSJ9Q2zKgnx7dfjbhXHtQgbCEZoWX3rhnGZ3CnNX2wkfB/1',
-  };
+    tokenURI:
+      'https://ikzttp.mypinata.cloud/ipfs/QmSJ9Q2zKgnx7dfjbhXHtQgbCEZoWX3rhnGZ3CnNX2wkfB/1',
+    maxMintPerWallet: 0
+  }
 
-  await badgeContract.createBadge(badgeArgs);
+  await badgeContract.createBadge(badgeArgs)
 
   const badgeArgs2 = {
     mintable: true,
     transferable: false,
     amount: ethers.utils.parseUnits('100', 18),
     maxSupply: 100,
-    tokenURI: 'https://ikzttp.mypinata.cloud/ipfs/QmSJ9Q2zKgnx7dfjbhXHtQgbCEZoWX3rhnGZ3CnNX2wkfB/1',
-  };
-  await badgeContract.createBadge(badgeArgs2);
+    tokenURI:
+      'https://ikzttp.mypinata.cloud/ipfs/QmSJ9Q2zKgnx7dfjbhXHtQgbCEZoWX3rhnGZ3CnNX2wkfB/1',
+    maxMintPerWallet: 2
+  }
+  await badgeContract.createBadge(badgeArgs2)
 
-  await badgeContract.transferOwnership('0xF3fb03B582bd19CfA6728FDED7e130aad396d99E')
+  await badgeContract.transferOwnership(
+    '0xF3fb03B582bd19CfA6728FDED7e130aad396d99E'
+  )
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
